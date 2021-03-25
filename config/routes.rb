35d8c92478307621db60d32a5db306588f87274d
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-03-25 20:34:46 +0900

    トップページフレーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'bikkes/index'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "bikkes#index"
+  resources :bikkes, only: [:index]
 end

6e20a11dd724a3cfe62129100a9834b93d0dcf0f
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-05-10 22:54:36 +0900

    ページネーション実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @reviews = current_user.reviews.includes({image_attachment: :blob}).order('created_at DESC')
+    @reviews = current_user.reviews.includes({image_attachment: :blob}).order('created_at DESC').page(params[:page]).per(5)
   end
 end

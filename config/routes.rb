cdfe7d629c93ec4cf50d311e45572e5ac908a6ba
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-05-10 22:37:05 +0900

    マイページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: "reviews#index"
   get '/reviews/displacement/:id', to: "reviews#displacement"
@@ -10,6 +11,7 @@ Rails.application.routes.draw do
     end
     resources :comments, only: :create
   end
+  resources :users, only: :show
 
   # ゲストログイン機能
   devise_scope :user do

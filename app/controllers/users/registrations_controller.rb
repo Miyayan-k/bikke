c27b364b88d697aa8681911b5283bcd37056d789
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-05-12 00:17:14 +0900

    現在のパスワードのみ入力不要にする

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: [:edit, :update]
-  
+
   protected
 
   def update_resource(resource, params)
-    resource.update_without_password(params)
+    resource.update_without_current_password(params)
   end
 
   #更新後のパスを指定

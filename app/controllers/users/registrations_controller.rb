5cb1d946217b6d6a28f94cf65e5ce863b58c50a5
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-05-11 21:16:35 +0900

    ゲストユーザーは編集させない

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-
+  before_action :ensure_normal_user, only: [:edit, :update]
+  
   protected
 
   def update_resource(resource, params)
@@ -9,5 +10,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #更新後のパスを指定
   def after_update_path_for(resource)
     user_path(@user.id)
+  end
+
+  #ゲストユーザーはアカウント情報を編集・更新できない
+  def ensure_normal_user
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーは編集できません。'
+    end
   end
 end

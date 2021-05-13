a62c5a8708268250a5534d0905dafb2afa173f51
Miyayan-k <kentaro.miyayan2077hornet@gmail.com>
2021-05-14 00:33:23 +0900

    名前修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_131654) do
+ActiveRecord::Schema.define(version: 2021_05_13_132819) do
 
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_131654) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "likes_count"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
